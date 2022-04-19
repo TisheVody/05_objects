ba93b48d2e6805d8d877fb7935cf64dbce80f3c8
TisheVody <lesvtornikutro@yandex.ru>
2022-04-19 15:10:11 +0300

    add Posts and WallServiceTest

--- src/main/kotlin/Posts.kt
+++ src/main/kotlin/Posts.kt
@@ -1,34 +1,55 @@
 class Posts {
 }
 
+fun main() {
+
+}
+
 data class Post(
     val id: Int,
     val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
+//    val fromId: Int,
+//    val createdBy: Int,
     val date: Int,
     val text: String,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
-    val friendsOnly: Boolean,
-    val comments: Object,
-    val copyright: Object,
-    val likes: Object,
-    val reposts: Object,
-    val views: Object,
-    val postType: String,
-    val signerId: Int,
-    val canPin: Boolean,
-    val canDelete: Boolean,
-    val canEdit: Boolean,
-    val isPinned: Int,
-    val markedAsAds: Boolean,
-    val isFavorite: Boolean,
-    val donut: Object,
-    val postponedId: Int,
+//    val replyOwnerId: Int,
+//    val replyPostId: Int,
+//    val friendsOnly: Boolean,
+//    val comments: Object,
+//    val copyright: Object,
+//    val likes: Object,
+//    val reposts: Object,
+//    val views: Object,
+//    val postType: String,
+//    val signerId: Int,
+//    val canPin: Boolean,
+//    val canDelete: Boolean,
+//    val canEdit: Boolean,
+//    val isPinned: Int,
+//    val markedAsAds: Boolean,
+//    val isFavorite: Boolean,
+//    val donut: Object,
+//    val postponedId: Int,
 )
 
 object WallService {
     private var posts = emptyArray<Post>()
+    private var property = 0
+
+    fun add(post: Post): Post {
+        property += 1
+        posts += post.copy(id = property)
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for (elm in posts) {
+            if (elm.id == post.id) {
+                elm.copy(text = post.text)
+                return true
+            }
+        }
+        return false
+    }
 }
 
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,70 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        // arrange
+        val testPost1 = Post(
+            id = 0,
+            0,
+            0,
+            "Тестим"
+        )
+
+        // assert
+        assertTrue(WallService.add(testPost1).id != 0)
+    }
+
+    @Test
+    fun update_true() {
+        // arrange
+        val testPost1 = Post(
+            0,
+            0,
+            0,
+            "Тестим"
+        )
+        WallService.add(testPost1)
+
+        val testPost2 = Post(
+            id = 1,
+            0,
+            0,
+            "updated"
+        )
+
+        // act
+        val actualResultUpd = WallService.update(testPost2)
+
+        // assert
+        assertEquals(true, actualResultUpd)
+    }
+
+    @Test
+    fun update_false() {
+        // arrange
+        val testPost1 = Post(
+            id = 12345,
+            0,
+            0,
+            "Тестим"
+        )
+        WallService.add(testPost1)
+
+        val testPost2 = Post(
+            id = 1234,
+            0,
+            0,
+            "updated"
+        )
+
+        // act
+        val actualResultUpd = WallService.update(testPost2)
+
+        // assert
+        assertEquals(false, actualResultUpd)
+    }
+}
